f3b81d6a905331a17f1298f086051c5698bd0fc2
squaredumplings <mihnea.buzoiu@gmail.com>
2019-11-23 19:04:36 +0200

    Work in progress

--- main.cpp
+++ main.cpp
@@ -1,156 +1,262 @@
 #include <iostream>
 #include <ncurses.h>
 #include <unistd.h>
+#include <math.h>
+
 using namespace std;
 
-int pieces[7][4][4] = {{{1, 1, 0, 0},
-                       {1, 1, 0, 0},
+struct Player
+{
+    int level;
+    int score;
+    int cleared_lines;
+    int lines;
+    int b[20][10];
+    int x, y;
+    int piece[4][4];
+};
+
+Player p1, p2;
+
+//int level=1, score=0, cleared_lines=0;
+
+int pieces[7][4][4] = {{{0, 0, 0, 0},
+                       {0, 1, 1, 0},
+                       {0, 1, 1, 0},
+                       {0, 0, 0, 0}},
+
+                      {{0, 0, 0, 0},
+                       {2, 2, 2, 2},
                        {0, 0, 0, 0},
                        {0, 0, 0, 0}},
 
-                      {{2, 2, 2, 2},
-                       {0, 0, 0, 0},
-                       {0, 0, 0, 0},
-                       {0, 0, 0, 0}},
-
-                      {{3, 3, 3, 0},
+                      {{0, 0, 0, 0},
+                       {3, 3, 3, 0},
                        {0, 0, 3, 0},
-                       {0, 0, 0, 0},
                        {0, 0, 0, 0}},
 
-                      {{0, 4, 0, 0},
-                       {4, 4, 0, 0},
-                       {4, 0, 0, 0},
+                      {{0, 0, 4, 0},
+                       {0, 4, 4, 0},
+                       {0, 4, 0, 0},
                        {0, 0, 0, 0}},
 
                       {{0, 5, 0, 0},
-                       {5, 5, 5, 0},
-                       {0, 0, 0, 0},
+                       {0, 5, 5, 0},
+                       {0, 5, 0, 0},
                        {0, 0, 0, 0}},
 
-                      {{0, 0, 6, 0},
-                       {6, 6, 6, 0},
-                       {0, 0, 0, 0},
+                      {{0, 6, 6, 0},
+                       {0, 0, 6, 0},
+                       {0, 0, 6, 0},
                        {0, 0, 0, 0}},
 
-                      {{7, 0, 0, 0},
-                       {7, 7, 0, 0},
-                       {0, 7, 0, 0},
+                      {{0, 7, 0, 0},
+                       {0, 7, 7, 0},
+                       {0, 0, 7, 0},
                        {0, 0, 0, 0}}};
 
-int width[7] = {2, 4, 3, 2, 3, 3, 2};
-
-void print_board(int x, int y, int p, int b[20][10]) {
+void print_board (Player p, int offset) {
     for (int i=0; i<20; i++)
     {
-        cout << "|";
+        move(i+1, 20+offset);
+        addch('|');
         for (int j=0; j<10; j++) {
-            if ( i >= y && i < y+4 &&
-                 j >= x && j < x+4 &&
-                 pieces[p][i-y][j-x] != 0)
-                cout << "[]";
-            else if (b[i][j]==0)
-                cout << "  ";
-            else cout <<"{}";
+            if ( i >= p.y && i < p.y+4 &&
+                 j >= p.x && j < p.x+4 &&
+                 p.piece[i-p.y][j-p.x] != 0)
+                {
+                    // current piece
+                    attron(COLOR_PAIR(p.piece[i-p.y][j-p.x]));
+                    printw("  ");
+                    attroff(COLOR_PAIR(p.piece[i-p.y][j-p.x]));
+                }
+            else if (p.b[i][j]==0)
+                // blank space
+                printw("  ");
+            else {
+                // stationary block
+                attron(COLOR_PAIR(p.b[i][j]));
+                printw("  ");
+                attroff(COLOR_PAIR(p.b[i][j]));
+            }
 
         }
-        cout << "|" << endl;
+        addch('|');
     }
+    move(21, 20+offset);
     for (int i=0; i<22; i++)
-        cout << "–";
-    cout << endl << endl;
+        addch('-');
+    move(22,0+offset);
+
+    mvprintw(1,0+offset, "Score: %d", p.score);
+    mvprintw(2,0+offset, "Level: %d", p.level);
+
+    refresh();
 }
 
-int random_column (int p)
-{
-    return rand() % (11 - width[p]);
-}
-
-void freeze_piece (int x, int y, int p, int b[20][10])
+void freeze_piece (Player p)
 {
     for (int i=0; i<4; i++)
     {
         for (int j=0; j<4; j++)
         {
-            if (pieces[p][i][j] != 0)
-                b[y+i][x+j] = p+1;
+            if (p.piece[i][j] != 0)
+                p.b[p.y+i][p.x+j] = p.piece[i][j];
         }
     }
 }
 
-bool piece_fits (int x, int y, int p, int b[20][10])
+bool piece_fits (int x, int y, int pc[4][4], int b[20][10])
 {
     bool yes = true;
     for (int i=0; i<4; i++)
     {
         for (int j=0; j<4; j++)
         {
-            if (pieces[p][i][j] != 0 && b[y+i][x+j] != 0)
+            if ((pc[i][j] != 0 && b[y+i][x+j] != 0) ||
+                (pc[i][j] != 0 && (x+j>=10 || x+j<0)) ||
+                (pc[i][j] != 0 && (y+i>=20 || y+i<0)) )
                 yes = false;
         }
     }
     return yes;
 }
 
+void copy_piece (int from[4][4], int to[4][4])
+{
+    for (int i=0; i<4; i++)
+    {
+        for (int j=0; j<4; j++){
+            to[i][j]=from[i][j];
+        }
+    }
+}
+
+void rotate_piece (int from[4][4], int to[4][4])
+{
+       for (int i=0; i<4; i++)
+    {
+        for (int j=0; j<4; j++)
+        {
+            to[i][j]=from[j][3-i];
+        }
+    }
+}
+
+int compute_score (int level, int lines)
+{
+    int scores[] = {0, 100, 400, 900, 2000};
+    int lev=level;
+
+    if (lev > 8)
+        lev = 8;
+
+    return (lev/2 + 1) * scores[lines];
+}
 
 
 int main() {
 
-    int x, y, b[20][10];
+    //int b[20][10];
 
     srand(time(NULL));
-    
+
     // empty board
     for (int i=0; i<20; i++){
         for (int j=0; j<10; j++)
-            b[i][j]=0;
+            p1.b[i][j]=p2.b[i][j]=0;
     }
+
+    initscr();
+    clear();
+    noecho();
+    cbreak();
+    keypad(stdscr, true);
+    timeout(0);
+    start_color();
+    for (int i=0; i<7; i++)
+        init_pair(i+1, i+1, i+1);
 
     // main game loop
     for (int k=0; k<150; k++)
     {
         // pick random piece and column
-        int p = rand() % 7;
-        x = random_column(p);
+        //int piece[4][4];
+        int q = rand() % 7;
+        copy_piece(pieces[q], p1.piece);
+        p1.x = 5;
 
         // fall piece loop
-        for (y = 0; y < 20 && piece_fits(x, y, p, b) ; y++)
+        //int y = 0;
+        bool falling = false;
+        while (piece_fits(p1.x, p1.y, p1.piece, p1.b))
         {
-            print_board(x, y, p, b);
-            usleep(500000);
+            for (int j=0; j<50; j++){
+                int c = getch();
+                switch (c)
+                {
+                    case KEY_UP:
+                    case 'w':       int rot[4][4]; rotate_piece(p1.piece, rot);
+                                    if (piece_fits(p1.x, p1.y, rot, p1.b)) copy_piece(rot, p1.piece); break;
+
+                    case KEY_RIGHT:
+                    case 'd':       if (piece_fits(p1.x+1, p1.y, p1.piece, p1.b)) p1.x++; break;
+
+                    case KEY_LEFT:
+                    case 'a':       if (piece_fits(p1.x-1, p1.y, p1.piece, p1.b)) p1.x--; break;
+
+                    case KEY_DOWN:
+                    case 's':
+                    case ' ':       falling = true; break;
+                }
+                print_board(p1, 0);
+                if (!falling)
+                    usleep(10000.0/sqrt(p1.level+1));
+            }
+            p1.y++;
         }
 
         // game over when new piece can not fit
-        if (y==0)
+        if (p1.y==0)
             return -1;
 
         // freeze piece in last position on the board
-        freeze_piece(x, y-1, p, b);
+        freeze_piece(p1);
 
-        // check if a row is full
-        bool full=true;
-        for (int xx = 0 ; xx < 10 && full ; xx++)
-        {
-            if ( b[y-1][xx] == 0 )
-                full=false;
-        }
+        p1.lines=0;
 
-        //clear line
-        if ( full )
+        for (int i=0 ; i<4 && p1.y+i<21; i++)
         {
-            for (int yy=y-1 ; yy > 0 ; yy--)
+            // check if a row is full
+            bool full=true;
+            for (int xx = 0 ; xx < 10 && full ; xx++)
             {
-                for (int xx=0 ; xx < 10 ; xx++)
-                {
-                    b[yy][xx] = b[yy-1][xx];
-                }
+                if ( p1.b[p1.y-1+i][xx] == 0 )
+                    full=false;
             }
-            usleep(1000000);
-            print_board(x, y, p, b);
-            usleep(3000000);
+
+            //clear line
+            if ( full )
+            {
+                for (int yy=p1.y-1+i ; yy > 0 ; yy--)
+                {
+                    for (int xx=0 ; xx < 10 ; xx++)
+                    {
+                        p1.b[yy][xx] = p1.b[yy-1][xx];
+                    }
+                }
+                p1.lines++;
+            }
         }
+        p1.cleared_lines += p1.lines;
+        if (p1.cleared_lines >= 5)
+        {
+            p1.level++;
+            p1.cleared_lines = 0;
+        }
+        p1.score += compute_score(p1.level, p1.lines);
     }
 
-
+    endwin();
 
 }
