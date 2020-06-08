dafd2e03e9dc89e3af6c17042ae652d26b937e2c
squaredumplings <mihnea.buzoiu@gmail.com>
2020-06-08 20:58:24 +0300

    Major improvements

--- main.cpp
+++ main.cpp
@@ -1,114 +1,111 @@
 #include <iostream>
-//#include <ncurses.h>
 #include <SDL.h>
+#include <SDL_ttf.h>
 #include <unistd.h>
 #include <math.h>
+#include "piece_shapes.h"
 
 using namespace std;
+
+#define MAX_PLAYERS 10
 
 struct Player
 {
     int level=1;
-    int score;
-    int cleared_lines;
-    bool falling;
+    int score=0;
+    int cleared_lines=0;
+    bool falling=false;
     int b[20][10];
     int x, y;
     int piece[4][4];
+    int drop_count=0;
+} p[MAX_PLAYERS];
 
-    int drop_count;
-};
 
-const int SCREEN_WIDTH=640;
-const int SCREEN_HEIGHT=480;
 
-int pieces[7][4][4] = {{{0, 0, 0, 0},
-                       {0, 1, 1, 0},
-                       {0, 1, 1, 0},
-                       {0, 0, 0, 0}},
 
-                      {{0, 0, 0, 0},
-                       {2, 2, 2, 2},
-                       {0, 0, 0, 0},
-                       {0, 0, 0, 0}},
+const int SCREEN_WIDTH=1160;
+const int SCREEN_HEIGHT=640;
+const int PLAYERS=2;
 
-                      {{0, 0, 0, 0},
-                       {3, 3, 3, 0},
-                       {0, 0, 3, 0},
-                       {0, 0, 0, 0}},
 
-                      {{0, 0, 4, 0},
-                       {0, 4, 4, 0},
-                       {0, 4, 0, 0},
-                       {0, 0, 0, 0}},
 
-                      {{0, 5, 0, 0},
-                       {0, 5, 5, 0},
-                       {0, 5, 0, 0},
-                       {0, 0, 0, 0}},
-
-                      {{0, 6, 6, 0},
-                       {0, 0, 6, 0},
-                       {0, 0, 6, 0},
-                       {0, 0, 0, 0}},
-
-                      {{0, 7, 0, 0},
-                       {0, 7, 7, 0},
-                       {0, 0, 7, 0},
-                       {0, 0, 0, 0}}};
-
-int colors[7][3]={{0xAA, 0xAA, 0xAA},
+int colors[7][3]={{0x00, 0xAA, 0xAA},
                   {0xAA, 0xAA, 0xAA},
-                  {0xAA, 0xAA, 0xAA},
-                  {0xAA, 0xAA, 0xAA},
-                  {0xAA, 0xAA, 0xAA},
-                  {0xAA, 0xAA, 0xAA},
-                  {0xAA, 0xAA, 0xAA}};
+                  {0xAA, 0x00, 0xAA},
+                  {0xAA, 0x34, 0x78},
+                  {0xAA, 0xAA, 0x44},
+                  {0x55, 0xAA, 0xAA},
+                  {0xAA, 0x99, 0xAA}};
 
-const int w=15;
+const int w=20;
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
-SDL_Surface* screenSurface = NULL;
+TTF_Font* Sans = NULL;
 
-void print_board (Player p, int offset) {
+void draw_text(char text[], int x, int y, TTF_Font* font) {
 
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-    SDL_RenderDrawLine(renderer, 13*w+offset*w, 0, 13*w+offset*w, 20*w);
-    SDL_RenderDrawLine(renderer, 13*w+offset*w, 20*w, 23*w+offset*w, 20*w);
-    SDL_RenderDrawLine(renderer, 23*w+offset*w, 20*w, 23*w+offset*w, 0);
+    SDL_Color White = {255, 255, 255};
+    SDL_Surface* surfaceMessage = TTF_RenderText_Blended(font, text, White);
+
+    SDL_Texture* Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
+
+    int texW = 0;
+    int texH = 0;
+    SDL_QueryTexture(Message, NULL, NULL, &texW, &texH);
+    SDL_Rect dstrect = { x, y, texW, texH };
+
+    SDL_RenderCopy(renderer, Message, NULL, &dstrect);
+
+    SDL_FreeSurface(surfaceMessage);
+    SDL_DestroyTexture(Message);
+
+}
+
+void print_board (Player p, int xoffset, int yoffset) {
+
+    SDL_SetRenderDrawColor (renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+    SDL_RenderDrawLine(renderer, xoffset*w-1, yoffset*w-1, xoffset*w-1, (22+yoffset)*w);
+    SDL_RenderDrawLine(renderer, (xoffset+4)*w-1, yoffset*w-1, (xoffset+4)*w-1, (22+yoffset)*w);
+    SDL_RenderDrawLine(renderer, (xoffset+14)*w, yoffset*w-1, (xoffset+14)*w, (22+yoffset)*w);
+
+    SDL_RenderDrawLine(renderer, xoffset*w-1, yoffset*w-1, (xoffset+14)*w-1, yoffset*w-1);
+    SDL_RenderDrawLine(renderer, xoffset*w-1, (yoffset+2)*w-1, (xoffset+14)*w-1, (yoffset+2)*w-1);
+    SDL_RenderDrawLine(renderer, xoffset*w-1, (yoffset+22)*w, (xoffset+14)*w-1, (yoffset+22)*w);
+
+    char txt[10];
+    sprintf(txt, "%9d", p.score);
+    draw_text(txt, (xoffset+6)*w, yoffset*w+3, Sans);
+
+    sprintf(txt, "%d", p.level);
+    draw_text(txt, (xoffset)*w, yoffset*w+3, Sans);
 
     for (int i=0; i<20; i++)
     {
         for (int j=0; j<10; j++) {
+            SDL_Rect r = {(j+xoffset+4)*w, (i+yoffset+2)*w, w, w};
             if ( i >= p.y && i < p.y+4 &&
                  j >= p.x && j < p.x+4 &&
                  p.piece[i-p.y][j-p.x] != 0)
             {
                 // current piece
-                int c = p.piece[i-p.y][j-p.x];
+                int c = p.piece[i-p.y][j-p.x]-1;
                 SDL_SetRenderDrawColor(renderer, colors[c][0], colors[c][1], colors[c][2], SDL_ALPHA_OPAQUE);
-                SDL_Rect r = {(13+j)*w+offset*w, i*w, w, w};
-                SDL_RenderFillRect(renderer, &r);
+
             }
             else if (p.b[i][j]==0)
             {
                 // blank space
                 SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-                SDL_Rect r = {(13+j)*w+offset*w, i*w, w, w};
-                SDL_RenderFillRect(renderer, &r);
             }
-
             else {
                 // stationary block
-                int c = p.b[i][j];
+                int c = p.b[i][j]-1;
                 SDL_SetRenderDrawColor(renderer, colors[c][0], colors[c][1], colors[c][2], SDL_ALPHA_OPAQUE);
-                SDL_Rect r = {(13+j)*w+offset*w, i*w, w, w};
-                SDL_RenderFillRect(renderer, &r);
             }
+            SDL_RenderFillRect(renderer, &r);
         }
     }
-
-    SDL_RenderPresent(renderer);
 }
 
 void freeze_piece (Player &p) {
@@ -166,7 +163,7 @@ int compute_score (int level, int lines) {
     return (lev/2 + 1) * scores[lines];
 }
 
-int clear_lines(Player &p) {
+int clear_lines (Player &p) {
     int lines=0;
     for (int i=0 ; i<20; i++)
     {
@@ -194,7 +191,7 @@ int clear_lines(Player &p) {
     return lines;
 }
 
-void random_piece(Player &p) {
+void random_piece (Player &p) {
     int q = rand() % 7;
     copy_piece(pieces[q], p.piece);
     p.x = 5;
@@ -202,7 +199,7 @@ void random_piece(Player &p) {
     p.falling = false;
 }
 
-void next_piece(Player &p) {
+void next_piece (Player &p) {
     // freeze piece in last position on the board
     freeze_piece(p);
 
@@ -220,14 +217,10 @@ void next_piece(Player &p) {
     random_piece(p);
 }
 
-int drop_speed(Player p) {
+int drop_speed (Player p) {
     if (p.falling) return 1;
     else return 30.0/sqrt(p.level+1);
 }
-
-
-#define PLAYERS 2
-Player p[PLAYERS];
 
 int main( int argc, char* args[] ) {
 
@@ -246,11 +239,14 @@ int main( int argc, char* args[] ) {
         return -1;
     }
 
-    if( SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_RESIZABLE, &window, &renderer ))
-    {
-        printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-        return -1;
+    if(TTF_Init()==-1) {
+        printf("TTF_Init: %s\n", TTF_GetError());
+        exit(2);
     }
+    Sans = TTF_OpenFont("Andale.ttf", 30);
+
+    window = SDL_CreateWindow("", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_RESIZABLE);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
     // pick random piece and column
     for (int k=0; k<PLAYERS; k++)
@@ -261,42 +257,61 @@ int main( int argc, char* args[] ) {
     {
         SDL_Event event;
         if (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
+
+            if (event.type == SDL_KEYDOWN){
+
+                switch (event.key.keysym.sym)
+                {
+                    case SDLK_w: {
+                        int rot[4][4];
+                        rotate_piece(p[0].piece, rot);
+                        if (piece_fits(p[0].x, p[0].y, rot, p[0].b))
+                            copy_piece(rot, p[0].piece);
+                        break;
+                    }
+
+                    case SDLK_UP: {
+                        int rot[4][4];
+                        rotate_piece(p[1].piece, rot);
+                        if (piece_fits(p[1].x, p[1].y, rot, p[1].b))
+                            copy_piece(rot, p[1].piece);
+                        break;
+                    }
+
+                    case SDLK_RIGHT: if (piece_fits(p[1].x+1, p[1].y, p[1].piece, p[1].b)) p[1].x++; break;
+                    case SDLK_d:       if (piece_fits(p[0].x+1, p[0].y, p[0].piece, p[0].b)) p[0].x++; break;
+
+                    case SDLK_LEFT:  if (piece_fits(p[1].x-1, p[1].y, p[1].piece, p[1].b)) p[1].x--; break;
+                    case SDLK_a:       if (piece_fits(p[0].x-1, p[0].y, p[0].piece, p[0].b)) p[0].x--; break;
+
+                    case SDLK_DOWN:  p[1].falling = true; break;
+                    case SDLK_s:
+                    case SDLK_SPACE:       p[0].falling = true; break;
+                }
+            }
+
+            else if (event.type == SDL_QUIT) {
                 break;
             }
         }
 
-/*        int c = getch();
-        switch (c)
-        {
-            case 'w':       {int rot[4][4]; rotate_piece(p[0].piece, rot);
-                            if (piece_fits(p[0].x, p[0].y, rot, p[0].b)) copy_piece(rot, p[0].piece); break;}
 
-            case KEY_UP:    {int rot[4][4]; rotate_piece(p[1].piece, rot);
-                            if (piece_fits(p[1].x, p[1].y, rot, p[1].b)) copy_piece(rot, p[1].piece); break;}
 
-            case KEY_RIGHT: if (piece_fits(p[1].x+1, p[1].y, p[1].piece, p[1].b)) p[1].x++; break;
-            case 'd':       if (piece_fits(p[0].x+1, p[0].y, p[0].piece, p[0].b)) p[0].x++; break;
-
-            case KEY_LEFT:  if (piece_fits(p[1].x-1, p[1].y, p[1].piece, p[1].b)) p[1].x--; break;
-            case 'a':       if (piece_fits(p[0].x-1, p[0].y, p[0].piece, p[0].b)) p[0].x--; break;
-
-            case KEY_DOWN:  p[1].falling = true; break;
-            case 's':
-            case ' ':       p[0].falling = true; break;
-        }*/
-
-//        SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-//        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+        SDL_RenderClear(renderer);
 
         for (int k=0; k<PLAYERS; k++)
-            print_board(p[k], 30 * k);
+            print_board(p[k], 4 + k*18, 5);
+
+        SDL_RenderPresent(renderer);
 
         for (int k=0; k<PLAYERS; k++)
         {
             if (!piece_fits(p[k].x, p[k].y+1, p[k].piece, p[k].b)) {
                 // game over when new piece can not fit
-                if (p[k].y == 0) return -2;
+                if (p[k].y == 0) {
+                    
+                }
 
                 next_piece(p[k]);
             }
